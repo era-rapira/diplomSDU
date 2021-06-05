3c901c2256e7d707ba26e69f6cc495103c8bec5f
vandervaiz <kol6asa.d@gmail.com>
2021-06-05 13:04:19 +0600

    style: refactor style bagFix: fix fetch

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -6,12 +6,77 @@ import banner2 from '../../assets/banner2.jpg';
 import banner3 from '../../assets/banner3.jpg';
 import {Carousel} from '3d-react-carousal';
 import './index.css';
-import { Grid } from '@material-ui/core';
+import { CircularProgress, fade, Grid, withStyles } from '@material-ui/core';
 import Product from '../Products/Product/Product';
 import ItemsCarousel from 'react-items-carousel';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import Footer from '../Footer/Footer';
 
-function Homepage() {
+const styles = theme => ({
+    root: {
+      width: '100%',
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing.unit,
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing.unit * 9,
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%',
+    },
+    inputInput: {
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit * 10,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  });
+
+function Homepage(props) {
+    const { classes } = props;
     const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
     const [cart, setCart] = useState({});
     const [order, setOrder] = useState({});
     const [errorMessage, setErrorMessage] = useState('');
@@ -25,6 +90,14 @@ function Homepage() {
         setProducts(data);
     }
 
+    const fetchProductsByQuery = async (query) => {
+        const { data } = await commerce.products.list({
+            query: `${query}`
+        });
+
+        setFilteredProducts(data ?? []);
+    }
+
     const fetchCart = async () => {
         setCart(await commerce.cart.retrieve())
     }
@@ -34,41 +107,7 @@ function Homepage() {
 
         setCart(cart);
     }
-
-    const handleUpdateCartQty = async (productId, quantity) => {
-        const { cart } = await commerce.cart.update(productId, { quantity });
-
-        setCart(cart);
-    }
-
-    const handleRemoveFromCart = async (productId) => {
-        const { cart } = await commerce.cart.remove(productId);
-
-        setCart(cart);
-    }
-
-    const handleEmptyCart = async () => {
-        const { cart } = await commerce.cart.empty();
-
-        setCart(cart);
-    }
-
-    const refreshCart = async () => {
-        const newCart = await commerce.cart.refresh();
-
-        setCart(newCart);
-    }
-
-    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
-        try {
-            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
-
-            setOrder(incomingOrder);
-            refreshCart();
-        } catch (error) {
-            setErrorMessage(error.data.error.message);
-        }
-    }
+  
 
     useEffect(() => {
         fetchProducts();
@@ -88,20 +127,39 @@ function Homepage() {
             <div className="carousel__content">
             <Carousel slides={slides} autoplay={true} interval={5000}/>
             </div>
+            <div className="homepage__filter">
+            <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              onChange={(event) => {
+                  console.log(event.target.value);
+                  fetchProductsByQuery(event.target.value)
+              }}
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
+            </div>
+            <Products products={filteredProducts.length > 0 ? filteredProducts : products} onAddToCart={handleAddToCart} />
             <div className="carousel__products">
             <ItemsCarousel
                 // Placeholder configurations
                 enablePlaceholder
                 numberOfPlaceholderItems={5}
                 minimumPlaceholderTime={1000}
-                placeholderItem={<div style={{ height: 200, background: '#900' }}>Placeholder</div>}
+                placeholderItem={<CircularProgress></CircularProgress>}
 
                 // Carousel configurations
-                numberOfCards={3}
+                numberOfCards={4}
                 gutter={12}
                 showSlither={true}
                 firstAndLastGutter={true}
-                freeScrolling={false}
+                freeScrolling={true}
 
                 // Active item configurations
                 requestToChangeActive={changeActiveItem}
@@ -109,22 +167,22 @@ function Homepage() {
                 activePosition={'center'}
 
                 chevronWidth={24}
-                rightChevron={'>'}
-                leftChevron={'<'}
+                rightChevron={<button style={{ width: '50px', position: 'absolute', backgroundColor: 'aqua'}}>{'>'}</button>}
+                leftChevron={<button style={{ width: '50px', position: 'absolute' , backgroundColor: 'aqua'}}>{'<'}</button>}
                 outsideChevron={false}
             >
                 {products.map((product) => (
                     <div className="carousel__products__item">
-<Grid item key={product.id} xs={12}>
+                    <Grid item key={product.id} xs={12}>
                         <Product product={product} onAddToCart={handleAddToCart} />
                     </Grid> 
                     </div>
                 ))}
             </ItemsCarousel>
             </div>
-            <Products products={products} onAddToCart={handleAddToCart} />
+            <Footer/>
         </div>
     )
 }
-
-export default Homepage
+  
+  export default withStyles(styles)(Homepage);
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { InfoOutlined, ShoppingCart } from '@material-ui/icons';
+import { InfoOutlined, ShoppingCart, Person } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom'
 
 import logo from '../../assets/commerce.png'
@@ -17,8 +17,8 @@ const Navbar = ({ totalItems }) => {
             <AppBar position="relative" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-                        <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
-                        Commerce.js
+                        <img src={logo} alt="Commerce" height="25px" className={classes.image} />
+                        Miracle Boutique
                     </Typography>
                     <div className={classes.grow} />
                     {location.pathname === '/' && (
@@ -27,6 +27,12 @@ const Navbar = ({ totalItems }) => {
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
+                        </IconButton>
+                    </div> )}
+                    {location.pathname === '/' && (
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/login" aria-label="Login" color="inherit">
+                                <Person />
                         </IconButton>
                     </div> )}
                 </Toolbar>
